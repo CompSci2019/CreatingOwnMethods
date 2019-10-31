7e650e74b1292b128e6feab5d9890ecdba375e74
rkaune <rkaune@7HPL6Y1.Student.UGDSB.ED>
2019-10-31 13:26:23 -0400

    Second commit. Added menu and calls to addAndOut() and multAndOut() in program. Also added return types to show flow from methods to main()

--- src/javaapplication4/JavaApplication4.java
+++ src/javaapplication4/JavaApplication4.java
@@ -14,31 +14,41 @@ public class JavaApplication4  {
 
         Scanner keyedInput = new Scanner (System.in);    
 
-        int num1, num2, sum, product;    
-
-        System.out.println("Enter the first number:");
-        num1 = keyedInput.nextInt();       
-
-        System.out.println("Enter the second number:");
-        num2 = keyedInput.nextInt();         
-
-        //sum = num1 + num2;
-        //product = num1 * num2;       
-
-        //System.out.println("The sum is: " + sum);
-        //System.out.println("The product is: " + product);
-        addAndOut(num1,num2);
-        multAndOut(num1,num2);
-
+        int num1, num2;  
+        int option = 0;
+        do {
+            System.out.println("Menu");
+            System.out.println("____");
+            System.out.println("1. Add the numbers together");
+            System.out.println("2. Multiply the numbers together");
+            option = keyedInput.nextInt();
+            if (option==1){
+                System.out.println("Enter the first number:");
+                num1 = keyedInput.nextInt();
+                System.out.println("Enter the second number:");
+                num2 = keyedInput.nextInt();
+                System.out.println("The answer is " + addAndOut(num1,num2));
+            }
+            else if (option==2){
+                System.out.println("Enter the first number:");
+                num1 = keyedInput.nextInt();
+                System.out.println("Enter the second number:");
+                num2 = keyedInput.nextInt(); 
+                System.out.println("The answer is " + multAndOut(num1,num2) );
+            }
+            
+        } while(option !=-1);
     } 
     
-    public static void addAndOut (int n1, int n2){
+    public static int addAndOut (int n1, int n2){
         int sum = n1+n2;
-        System.out.println("The sum is " + sum);
+        //System.out.println("The sum is " + sum);
+        return sum;
     }
     
-    public static void multAndOut (int n1, int n2){
+    public static int multAndOut (int n1, int n2){
         int product = n1*n2;
-        System.out.println("The product is " + product);
+        //System.out.println("The product is " + product);
+        return product;
     }
 }
